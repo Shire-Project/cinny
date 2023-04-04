a76a07a9a6e05b1451e1eaf51f2b46442cf7490c
Ahmed Korani <ahmed@shire.blue>
2023-04-04 05:12:39 +0200

    Add new implementation to handle creating or restoring backups for current or new users

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -5,7 +5,11 @@ import { isAuthenticated } from '../../client/state/auth';
 import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
 
+import { CrossSigningEventHandler, MessagesBackupEventHandler } from '../../shire/EventsChannel';
+
 function App() {
+  CrossSigningEventHandler();
+  MessagesBackupEventHandler();
   return isAuthenticated() ? <Client /> : <Auth />;
 }
 
